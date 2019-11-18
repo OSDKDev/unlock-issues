3e548cda577304153c76cce6cc39c1903b2b7794
Nacho <hello@dunningkruger.dev>
2019-11-18 13:54:52 -0500

    Update unlock.js

--- src/unlock.js
+++ src/unlock.js
@@ -5,14 +5,20 @@ async function unlock() {
   const myToken = core.getInput("repo-token");
   const octokit = new github.GitHub(myToken);
   const context = github.context;
+  
+  const issue = core.getInput("issue-number") || context.payload.issue.number;
+  const repo = core.getInput("repo-name") || context.payload.repository.name;
+  const owner = core.getInput("owner-name") || context.payload.repository.owner.login;
 
+  console.log("Attempting to unlock " + owner + "/" + repo + "/issues/" + issue);
+  
   await octokit.issues.unlock({
-    owner: context.payload.repository.owner.login,
-    repo: context.payload.repository.name,
-    issue_number: context.payload.issue.number
+    owner: owner,
+    repo: repo,
+    issue_number: parseInt(issue)
   });
   
-  return context.payload.issue.number;
+  return issue;
 }
 
 unlock()
